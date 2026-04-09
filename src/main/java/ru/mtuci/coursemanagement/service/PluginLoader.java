61f2f9335477033445481fb0d5038b7abed4cd00
Евгений Плешивцев <e.v.g.pl@mail.ru>
2026-04-09 17:51:33 +0300

    practic

--- src/main/java/ru/mtuci/coursemanagement/service/PluginLoader.java
+++ src/main/java/ru/mtuci/coursemanagement/service/PluginLoader.java
@@ -1,12 +1,15 @@
 package ru.mtuci.coursemanagement.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class PluginLoader {
 
+    // Загрузка плагинов из произвольных URL отключена
+    // как небезопасная функциональность
     public void tryLoad() {
-        // TODO: реализовать загрузку плагинов
-        System.out.println("Loading plugins...");
+        log.info("Plugin loading from external URLs is disabled for security reasons");
     }
 }
